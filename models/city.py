bd2d1319a49824c8da9ea1ed4eeca599f4e2dff3
Ayobami <ayobamidele006@gmail.com>
2023-04-19 18:04:46 +0100

    convert some classes to orm models

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -13,3 +14,4 @@ class City(BaseModel, Base):
     state_id = Column(String(60),
                       ForeignKey("states.id", ondelete="CASCADE"),
                       nullable=False)
+    places = relationship("Place", backref="cities", cascade="all, delete")
