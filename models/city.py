865896babc18b855646f73ebbb9ba7ec308202f0
Jojo <jojothomas1515@gmail.com>
2023-04-19 11:49:11 +0100

    sqlalchemy class to table map implemented for state

--- models/city.py
+++ models/city.py
@@ -11,5 +11,5 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60),
-                      ForeignKey("states.id"),
+                      ForeignKey("states.id", ondelete="CASCADE"),
                       nullable=False)
