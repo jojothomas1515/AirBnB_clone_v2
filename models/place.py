44cfafbb0b45ab62e00a451c0873797c89654cb2
Jojo <jojothomas1515@gmail.com>
2023-04-20 11:43:25 +0100

    tasks 10 with relaionships

--- models/place.py
+++ models/place.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
+"""Place Module for HBNB project."""
 from models.base_model import BaseModel, Base
+import models
+from models.amenity import Amenity
 from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
-from os import getenv
+from os import getenv, environ
+
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id",
+                             String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True),
+                      Column("amenity_id",
+                             String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True))
 
 
 class Place(BaseModel, Base):
-    """ A place to stay """
+    """A place to stay."""
+
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
@@ -37,3 +50,29 @@ class Place(BaseModel, Base):
     #             if review.place_id == self.id:
     #                 review_list.append(review)
     #         return review_list
+
+    if environ.get("HBNB_TYPE_STORAGE") == "db":
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False, backref="place_amenities")
+    else:
+        @property
+        def amenities(self) -> list:
+            """Return the list of amenities instance.
+
+            The amenities instance returned is based on the
+            amenities id stored on the list variable amenity_ids.
+            """
+            amenities_list = []
+            for amenity in models.storage.all(Amenity).values():
+                for ids in self.amenity_ids:
+                    if ids == amenity.id:
+                        amenities_list.append(amenity)
+            return amenities_list
+
+        @amenities.setter
+        def amenities(self, obj: Amenity):
+            """Add the amenity obj instance id to amenity_ids."""
+            if type(obj) != Amenity or obj is None:
+                return
+            else:
+                self.amenity_ids.append(obj.id)
