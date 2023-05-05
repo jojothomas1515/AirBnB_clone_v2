2c61994f388b284cbee44121239dabf6de6f4eb8
Jojo <jojothomas1515@gmail.com>
2023-05-05 11:52:00 +0100

    deploy web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -30,26 +30,29 @@ def do_deploy(archive_path: str) -> str or None:
     arguments:
         archive_path: the path of the archive to be sent.
     """
+    try:
+        a_path = pl.Path(archive_path).absolute()
+        name = pl.Path(a_path).name
+        dest = "/data/web_static/releases/{}/".format(
+            name.split('.')[0])
+        link = "/data/web_static/current"
 
-    a_path = pl.Path(archive_path).absolute()
-    name = pl.Path(a_path).name
-    dest = "/data/web_static/releases/{}/".format(
-        name.split('.')[0])
-    link = "/data/web_static/current"
+        if not pl.Path(a_path).is_file():
+            return None
 
-    if not pl.Path(a_path).is_file():
-        return None
+        put(a_path, '/tmp/', use_sudo=True)
 
-    put(a_path, '/tmp/', use_sudo=True)
-
-    sudo("mkdir -p {path}".format(path=dest))
-    sudo("tar xf /tmp/{name} -C {dest}"
-         .format(name=name, dest=dest))
-    sudo("rm -r /tmp/{name}".format(name=name))
-    sudo("rm -rf /data/web_static/current")
-    sudo("ln -s {dest} {link} -f"
-         .format(dest=dest, link=link))
-    sudo("mv -u {dest}web_static/* {dest}"
-         .format(dest=dest))
-    sudo("rm -r {dest}web_static/"
-         .format(dest=dest))
+        sudo("mkdir -p {path}".format(path=dest))
+        sudo("tar xf /tmp/{name} -C {dest}"
+             .format(name=name, dest=dest))
+        sudo("rm -r /tmp/{name}".format(name=name))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s {dest} {link} -f"
+             .format(dest=dest, link=link))
+        sudo("mv -u {dest}web_static/* {dest}"
+             .format(dest=dest))
+        sudo("rm -r {dest}web_static/"
+             .format(dest=dest))
+        return True
+    except Exception:
+        return False
