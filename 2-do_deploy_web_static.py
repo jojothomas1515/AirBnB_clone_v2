d5a30ce556cb981a223116e1d1f746553b0ff92a
Jojo <jojothomas1515@gmail.com>
2023-05-05 11:34:23 +0100

    deploy web_static

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Distribute the compressed version of  web_static\
+folder of your AirBnB Clone repo."""
+from fabric.api import local, env, sudo, put
+import time
+import pathlib as pl
+import os
+
+env.hosts = ['34.205.65.154', '34.239.107.150']
+env.user = 'ubuntu'
+
+
+def do_pack():
+    """Compresses web static and add it to versions."""
+    if not pl.Path("versions").is_dir():
+        os.makedirs("versions")
+    mypath = "versions/web_static_{}.tgz".format(
+        time.strftime("%Y%m%d%H%M%S"))
+    res = local("tar caf {paths} web_static/"
+                .format(paths=mypath), )
+
+    if not pl.Path(mypath).is_file():
+        return None
+    return pl.Path(mypath).absolute()
+
+
+def do_deploy(archive_path: str) -> str or None:
+    """Push a specified archive in my server.
+
+    arguments:
+        archive_path: the path of the archive to be sent.
+    """
+
+    a_path = pl.Path(archive_path).absolute()
+    name = pl.Path(a_path).name
+    dest = "/data/web_static/releases/{}/".format(
+        name.split('.')[0])
+    link = "/data/web_static/current"
+
+    if not pl.Path(a_path).is_file():
+        return None
+
+    put(a_path, '/tmp/', use_sudo=True)
+
+    sudo("mkdir -p {path}".format(path=dest))
+    sudo("tar xf /tmp/{name} -C {dest}"
+         .format(name=name, dest=dest))
+    sudo("rm -r /tmp/{name}".format(name=name))
+    sudo("rm -rf /data/web_static/current")
+    sudo("ln -s {dest} {link} -f"
+         .format(dest=dest, link=link))
+    sudo("mv -u {dest}web_static/* {dest}"
+         .format(dest=dest))
+    sudo("rm -r {dest}web_static/"
+         .format(dest=dest))
