640ab192092a8fae804fe4e773b7a6ffc22a8144
Jojo <jojothomas1515@gmail.com>
2023-05-05 08:27:09 +0100

    comprex web_static file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Generates a .tgz archive from the contents of the web_static\
+folder of your AirBnB Clone repo."""
+from fabric.api import run, local
+import time
+import pathlib as pl
+import os
+
+
+def do_pack():
+    if not pl.Path("versions").is_dir():
+        os.makedirs("versions")
+    mypath = "versions/web_static_{}.tgz".format(
+        time.strftime("%Y%m%d%H%M%S"))
+    res = local("tar caf {paths} web_static/"
+                .format(paths=mypath), )
+
+    if not pl.Path(mypath).is_file():
+        return None
+    return pl.Path(mypath).absolute()
