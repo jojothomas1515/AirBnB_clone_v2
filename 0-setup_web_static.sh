5866b38c5352d56b212eac4167e77d9a5b5dbdc1
Jojo <jojothomas1515@gmail.com>
2023-05-06 15:32:54 +0100

    airbnb clone

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,6 +9,15 @@ then
     service nginx start
 fi
 
+redirect="\tlocation /redirect_me {\n\t\treturn 301 https://jojoport.netlify.com;\n\t}"
+error="\terror_page 404 /not_found.html;"   
+echo  "Hello World!" > /var/www/html/index.html
+echo "Ceci n'est pas une page" > /var/www/html/not_found.html
+sed -i "/server_name _;/a\ \n${redirect}\n" /etc/nginx/sites-available/default
+sed -i "/server_name _;/a\ \n${error}\n" /etc/nginx/sites-available/default
+sed -i "/server_name _;/a\ \tadd_header X-Served-By \$HOSTNAME always;" /etc/nginx/sites-available/default                                                            
+ 
+
 # data to test nnginx with
 fakeData="<html>
   <head>
@@ -23,14 +32,9 @@ mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 
 echo -e "$fakeData" > /data/web_static/releases/test/index.html
-
 ln -s /data/web_static/releases/test/ /data/web_static/current -f
-
 chown -R ubuntu:ubuntu /data/
-
 sed -i "/server_name _;/a\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
-
-sed -i "/server_name _;/a\ \tadd_header X-Served-By \$HOSTNAME always;" /etc/nginx/sites-available/default
 
 if (( "$(pgrep -c nginx)" <= "0"))
 then
