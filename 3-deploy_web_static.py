80787b99b277d503635819b40bf953cd04f23385
Jojo <jojothomas1515@gmail.com>
2023-05-20 15:06:29 +0100

    deploy web_static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -30,28 +30,23 @@ def do_deploy(archive_path: str) -> str or None:
     arguments:
         archive_path: the path of the archive to be sent.
     """
+    if not pl.Path(archive_path).is_file():
+        return False
+
+    filenamew = archive_path.split(".")[0].split("/")[-1]
     try:
-        a_path = pl.Path(archive_path).absolute()
-        name = pl.Path(a_path).name
-        dest = "/data/web_static/releases/{}/".format(
-            name.split('.')[0])
-        link = "/data/web_static/current"
-
-        if not pl.Path(a_path).is_file():
-            return None
-
-        put(a_path, '/tmp/', use_sudo=True)
-        run("mkdir -p {path}".format(path=dest))
-        run("tar -xzf /tmp/{name} -C {dest}"
-            .format(name=name, dest=dest))
-        run("rm /tmp/{name}".format(name=name))
-        run("mv {dest}web_static/* {dest}"
-            .format(dest=dest))
+        ans = put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/{}".format(filenamew))
+        run("tar -xf {} -C /data/web_static/releases/{}".format(ans[0],
+                                                                filenamew))
+        run("mv /data/web_static/releases/{name}/web_static/*"
+            " /data/web_static/releases/{name}/"
+            .format(name=filenamew))
+        run("rm {}".format(ans[0]))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(filenamew))
         run("rm -rf /data/web_static/current")
-        run("rm -rf {dest}web_static"
-            .format(dest=dest))
-        run("ln -sf {dest} {link}"
-            .format(dest=dest, link=link))
+        run("ln -s /data/web_static/releases/{} /data/web_static/current"
+            .format(filenamew))
         return True
     except Exception:
         return False
