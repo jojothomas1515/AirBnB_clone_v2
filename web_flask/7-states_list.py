d5d96d0a4d572a75cff0b6779379511714d09e28
Jojo <jojothomas1515@gmail.com>
2023-05-21 12:58:50 +0100

    checker failing

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,47 +9,10 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello():
-    """Hello Route."""
-    return ("Hello HBNB!")
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """Returns HBNB."""
-    return ("HBNB")
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def c_text(text: str):
-    """Somewhat like a loopback."""
-    return ("C {}".format(text.replace("_", " ")))
-
-
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python_text(text: str = "is cool"):
-    """Somewhat like a loopback."""
-    return ("Python {}".format(text.replace("_", " ")))
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def numb(n: int):
-    """N is a number."""
-    return ("{} is a number".format(n))
-
-
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def number_template(n: int):
-    """Returns a number template."""
-    return (render_template("5-number.html", number=n))
-
-
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def even_odd(n: int):
-    """Returns a template."""
-    return (render_template("6-number_odd_or_even.html", num=n))
+@app.teardown_appcontext
+def close_session(exception):
+    """Closes database session."""
+    storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
@@ -60,11 +23,5 @@ def states_list():
     return (render_template("7-states_list.html", datas=datas))
 
 
-@app.teardown_appcontext
-def close_session(exception):
-    """Closes database session."""
-    storage.close()
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
