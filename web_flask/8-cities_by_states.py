45d4bb1de70db337d98bc9f6e1d19eabf3e06c2b
Jojo <jojothomas1515@gmail.com>
2023-05-22 16:15:17 +0100

    checker failing

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -6,8 +6,8 @@ white route called cities by states.
 """
 
 from flask import Flask, render_template
-from models import storage
-from models.state import State
+# from models import storage
+# from models.state import State
 
 app = Flask(__name__)
 
@@ -15,14 +15,16 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_list_by_states():
     """Send all the cities associated with a state."""
-    data = storage.all(State)
+    # data = storage.all(State)
+    data = ["jojo", "thomas"]
     return render_template("8-cities_by_states.html", datas=data)
 
 
 @app.teardown_appcontext
 def teardown_session(exceptions):
     """Close the session after every requests."""
-    storage.close()
+    # storage.close()
+    pass
 
 
 if __name__ == '__main__':
